214a82f709aa7b607b42c4ff1385d0954344f60c
IIMarietteII <136766093+IIMarietteII@users.noreply.github.com>
2025-06-13 11:03:26 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# RSS Newsletter Project
+
+This project fetches RSS feeds and creates a newsletter-style webpage.
---
+++ feeds.py
@@ -0,0 +1,5 @@
+RSS_FEEDS = {
+    "Korea": "https://www.mediatoday.co.kr/rss/allArticle.xml",    
+    "Global": "https://www.adweek.com/feed/",
+    "A+E": "https://cdn.feedcontrol.net/10476/18830-Gn9fHlEmknBGj.xml"
+}
