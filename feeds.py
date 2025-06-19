cdb7d5caa60e3f54fe55a4382021cfb1178baf4f
IIMarietteII <136766093+IIMarietteII@users.noreply.github.com>
2025-06-19 17:17:33 +0900

    Update feeds.py

--- feeds.py
+++ feeds.py
@@ -1,14 +1,20 @@
 RSS_FEEDS = {
-    "Google 예능콘텐츠": "https://www.google.com/alerts/feeds/09227588750988725930/5874155639261198456",
-    "Google 드라마제작": "https://www.google.com/alerts/feeds/09227588750988725930/18115601270859787318",
-    "Google 드라마투자": "https://www.google.com/alerts/feeds/09227588750988725930/15054667442391058200",
-    "Google OTT": "https://www.google.com/alerts/feeds/09227588750988725930/15054667442391059191",
-    "Google 한류": "https://www.google.com/alerts/feeds/09227588750988725930/5874155639261199243",
-    "Google K드라마": "https://www.google.com/alerts/feeds/09227588750988725930/15078260007095676809",
-    "Google AI콘텐츠": "https://www.google.com/alerts/feeds/09227588750988725930/14256239503855192850",
-    "Google 네고왕": "https://www.google.com/alerts/feeds/09227588750988725930/8594056422529263421",
-
-    "Korea": "https://www.mediatoday.co.kr/rss/allArticle.xml",    
-    "Global": "https://www.adweek.com/feed/",
-    "A+E": "https://cdn.feedcontrol.net/10476/18830-Gn9fHlEmknBGj.xml"
+    "Korea_Entertainment_Contents": [
+        "https://www.google.com/alerts/feeds/09227588750988725930/5874155639261198456",
+        "https://www.google.com/alerts/feeds/09227588750988725930/8594056422529263421"
+    ],
+    "Korea_OTT": [
+        "https://www.google.com/alerts/feeds/09227588750988725930/18115601270859787318",
+        "https://www.google.com/alerts/feeds/09227588750988725930/15054667442391058200",
+        "https://www.google.com/alerts/feeds/09227588750988725930/15054667442391059191",
+	"https://www.google.com/alerts/feeds/09227588750988725930/5874155639261199243",
+	"https://www.google.com/alerts/feeds/09227588750988725930/15078260007095676809"
+    ],
+    "AI": [
+        "https://www.google.com/alerts/feeds/09227588750988725930/14256239503855192850"
+    ],
+    "News": [
+        "https://www.mediatoday.co.kr/rss/allArticle.xml",
+        "https://www.adweek.com/feed/"
+    ]
 }
